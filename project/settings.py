c026ecb509eed4f15e3b73e3a85c8f89c360374d
nankuF <fireloki87@gmail.com>
2022-04-16 11:14:00 +0300

    fixed ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ DEBUG = os.getenv('DEBUG', False)
 
 ROOT_URLCONF = 'project.urls'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
